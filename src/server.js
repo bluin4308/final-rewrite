8986126102587394252629b20350f9cc17f933f9
Volodymyr Ukraintsev <ukraincev.vova@gmail.com>
2022-11-17 02:54:50 +0200

    add express GZIP compression

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const compression = require("compression");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(compression());
 app.use("/", express.static(path.join(__dirname, "../dist")));
 
 app.get("/*", (_req, res) => {
