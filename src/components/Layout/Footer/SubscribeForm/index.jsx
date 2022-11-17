64c5101983e719958eebeff97fe9343ca12856b2
Volodymyr Ukraintsev <ukraincev.vova@gmail.com>
2022-11-17 05:17:48 +0200

    refactor

---
+++ src/components/Layout/Footer/SubscribeForm/index.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Formik } from "formik";
+
+export default function SubscribeForm() {
+  return (
+    <Formik
+      initialValues={{ email: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        // setTimeout(() => {
+        //   alert(JSON.stringify(values, null, 2));
+        //   setSubmitting(false);
+        // }, 400);
+        const data = new FormData();
+        data.append("email", values.email);
+        fetch(
+          "https://dsa1mc.wp4.host/wp-json/contact-form-7/v1/contact-forms/81/feedback",
+          {
+            method: "POST",
+            body: data,
+          }
+        ).then((res) => {
+          if (res.ok) {
+            alert("All ok your message was sent");
+            setSubmitting(false);
+          } else {
+            alert("Something goes wrong");
+            setSubmitting(false);
+          }
+        });
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        isValid,
+      }) => (
+        <form onSubmit={handleSubmit} className="subscribe-form">
+          <p className="label">Subscribe</p>
+          <p>To get special offers and VIP treatment:</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email here"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+            className={errors.email && touched.email ? "error" : ""}
+          />
+          <button
+            type="submit"
+            disabled={!isValid || errors.email || isSubmitting}
+          >
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  );
+}
