54c960f3d1106cb035b5faa91d27ad7d85857f4d
Volodymyr Ukraintsev <ukraincev.vova@gmail.com>
2022-11-23 16:55:27 +0200

    about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navigation from "../Navigation";
+import { useTitle } from "../../store";
 import "./style.scss";
 
 export default function About() {
+  const { setTitle } = useTitle();
+  useEffect(() => {
+    setTitle("About");
+  }, []);
+
   return (
     <div className="about">
-      <p>
-        Our LOGO store was founded in 2022. We sell two categories of clothing:
-        jeans and jackets of different clothing styles for customers of all ages
-        and types. We will be glad if you buy something in our store!
-      </p>
+      <Navigation />
+      <div className="description">
+        <p className="logo">logo</p>
+        <p>
+          Our LOGO store was founded in 2022. We sell two categories of
+          clothing: jeans and jackets of different clothing styles for customers
+          of all ages and types. We will be glad if you buy something in our
+          store!
+        </p>
+      </div>
     </div>
   );
 }
