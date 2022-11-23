dd088b6c5276e45799770f5c6233d6be82bb3db7
Volodymyr Ukraintsev <ukraincev.vova@gmail.com>
2022-11-23 04:18:35 +0200

    add about

---
+++ src/components/About/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+export default function About() {
+  return (
+    <div className="about">
+      <p>
+        Our LOGO store was founded in 2022. We sell two categories of clothing:
+        jeans and jackets of different clothing styles for customers of all ages
+        and types. We will be glad if you buy something in our store!
+      </p>
+    </div>
+  );
+}
