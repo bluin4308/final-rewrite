b3215f47e2ef6af8f650711e5e10b35b7964ebde
club <noemail@noemail.noemail>
2022-11-26 14:55:37 +0200

    checkout in progress

---
+++ src/components/Cart/Checkout/index.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { Formik } from "formik";
+import useStore from "../../../store";
+
+export default function Checkout() {
+  const { clothes } = useStore();
+
+  const totalSum = clothes.reduce((acc, obj) => {
+    return acc + parseInt(obj.amount);
+  }, 0);
+
+  return (
+    <div className="checkout">
+      <p className="form-title">Offer form</p>
+      <Formik
+        initialValues={{
+          username: "",
+          useremail: "",
+          address: "",
+          zip: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.usermail = true;
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.usermail = true;
+          }
+          if (!values.username) {
+            errors.username = true;
+          }
+          if (!values.address) {
+            errors.address = true;
+          }
+          if (!values.subject) {
+            errors.subject = "Required";
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          const data = new FormData();
+          data.append("username", values.username);
+          data.append("useremail", values.useremail);
+          data.append("address", values.address);
+          data.append("zip", values.zip);
+          data.append("total", totalSum.toFixed(2));
+          data.append("clothes", clothes);
+          fetch(
+            "https://dsa1mc.wp4.host/wp-json/contact-form-7/v1/contact-forms/91/feedback",
+            {
+              method: "POST",
+              body: data,
+            }
+          ).then((res) => {
+            if (res.ok) {
+              alert("Your order was sent!");
+              setSubmitting(false);
+            } else {
+              alert("Something went wrong!");
+              setSubmitting(false);
+            }
+          });
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          isValid,
+          dirty,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="input-section">
+              <label htmlFor="username">Full name</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter your full name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.username}
+                className={errors.username && touched.username ? "error" : ""}
+              />
+            </div>
+            <div className="input-section">
+              <label htmlFor="useremail">E-mail</label>
+              <input
+                type="email"
+                name="useremail"
+                id="useremail"
+                placeholder="Enter email here"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.useremail}
+                className={errors.useremail && touched.useremail ? "error" : ""}
+              />
+            </div>
+            <div className="input-section">
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                placeholder="Enter your address"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.address}
+                className={errors.address && touched.address ? "error" : ""}
+              />
+            </div>
+            <div className="input-section">
+              <label htmlFor="zip">ZIP-code</label>
+              <input
+                type="number"
+                name="zip"
+                id="zip"
+                placeholder="Your location zip code"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.zip}
+                className={errors.zip && touched.zip ? "error" : ""}
+              />
+            </div>
+            <button type="submit" disabled={!isValid || isSubmitting || !dirty}>
+              Buy
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+}
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -5,7 +5,23 @@ import { GET_CART_ITEMS } from "../../apollo";
 import Navigation from "../Navigation";
 import Card from "./Card";
 import NetworkError from "../NetworkError";
+import Checkout from "./Checkout";
 import "./style.scss";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        <div className="lds-ring">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function Cart() {
   const { clothes } = useStore();
@@ -27,29 +43,26 @@ export default function Cart() {
     <div className="cart-layout">
       <Navigation />
       <div className="_80percent">
+        {/* EMPTY CART */}
         {clothes.length === 0 && !error && (
           <p className="empty-cart">Your cart is empty.</p>
         )}
-        {clothes.length > 0 &&
-          !loading &&
-          !error &&
-          data &&
-          data.clothes.nodes.map((item, index) => {
-            return <Card data={item} key={index} />;
-          })}
-        {!loading && !!error && <NetworkError />}
-        {loading && !data && clothes.length > 0 && (
-          <div className="loader-container">
-            <div className="loader">
-              <div className="lds-ring">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            </div>
-          </div>
+
+        {/* WHEN DATA IS LOADED */}
+        {clothes.length > 0 && !loading && !error && data && (
+          <>
+            {data.clothes.nodes.map((item, index) => {
+              return <Card data={item} key={index} />;
+            })}
+            <Checkout />
+          </>
         )}
+
+        {/* ERROR BOUNDARY */}
+        {!loading && !!error && <NetworkError />}
+
+        {/* LOADING ANIMATION */}
+        {loading && !data && clothes.length > 0 && <Loader />}
       </div>
     </div>
   );
