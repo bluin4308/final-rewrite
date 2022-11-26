97580d418e169f21dd3e8716593ea608a927a9d9
club 2 <->
2022-11-26 19:20:31 +0200

    scroll to top component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo";
 import { BrowserRouter } from "react-router-dom";
+import { ScrollToTop } from "./helpers";
 import Routing from "./components/Routing";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ApolloProvider client={client}>
+        <ScrollToTop />
         <Routing />
       </ApolloProvider>
     </BrowserRouter>
