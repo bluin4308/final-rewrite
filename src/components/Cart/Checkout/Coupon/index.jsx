9b1939c09d99aa591deefe751dac30c915075100
club <noemail@noemail.noemail>
2022-11-27 20:25:51 +0200

    checkout coupon in progress

---
+++ src/components/Cart/Checkout/Coupon/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { GET_COUPON } from "../../../../apollo";
+
+export default function Coupon({ setCouponDiscount }) {
+  const [couponTitle, setCouponTitle] = useState("");
+  const [doCouponQuery, { data, loading }] = useLazyQuery(GET_COUPON, {
+    fetchPolicy: "no-cache",
+  });
+
+  useEffect(() => {
+    if (couponTitle.trim() !== "") {
+      doCouponQuery({
+        variables: {
+          title: couponTitle.trim(),
+        },
+      });
+    }
+  }, [couponTitle]);
+
+  useEffect(() => {
+    if (data && !loading && data.coupons.nodes[0]) {
+      setCouponDiscount(data.coupons.nodes[0].customfields.discount);
+    }
+  }, [loading]);
+
+  return (
+    <div className="get-coupon">
+      <p>Input coupon code and get your discount!</p>
+      <input
+        type="text"
+        value={couponTitle}
+        onChange={(e) => {
+          setCouponTitle(e.target.value.trim());
+        }}
+      />
+    </div>
+  );
+}
--- src/components/Cart/Checkout/index.jsx
+++ src/components/Cart/Checkout/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Formik } from "formik";
 import { Persist } from "formik-persist";
 import useStore from "../../../store";
+import Coupon from "./Coupon";
 
 const composeString = (arr) => {
   const result = arr.map(
@@ -24,150 +25,156 @@ export default function Checkout() {
   }, 0);
 
   const [total, setTotal] = useState(totalSum);
-
+  const [couponDiscount, setCouponDiscount] = useState(0);
+  console.log(couponDiscount);
   useEffect(() => {
     setTotal(totalSum);
   }, [totalSum]);
 
   return (
-    <div className="checkout">
-      <p className="form-title">Offer form</p>
-      <Formik
-        initialValues={{
-          username: "",
-          useremail: "",
-          address: "",
-          zip: "",
-        }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.useremail) {
-            errors.useremail = true;
-          }
-
-          if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.useremail)
-          ) {
-            errors.useremail = true;
-          }
-
-          if (!values.username) {
-            errors.username = true;
-          }
-
-          if (!values.address) {
-            errors.address = true;
-          }
-
-          if (!values.zip) {
-            errors.zip = true;
-          } else if (/\D/.test(values.zip)) {
-            errors.zip = true;
-          }
-
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          const data = new FormData();
-          data.append("username", values.username);
-          data.append("useremail", values.useremail);
-          data.append("address", values.address);
-          data.append("zip", values.zip);
-          data.append("total", total.toFixed(2));
-          data.append("clothes", composeString(clothes));
-          fetch(
-            "https://dsa1mc.wp4.host/wp-json/contact-form-7/v1/contact-forms/91/feedback",
-            {
-              method: "POST",
-              body: data,
+    <>
+      <div className="checkout">
+        <p className="form-title">Offer form</p>
+        <Formik
+          initialValues={{
+            username: "",
+            useremail: "",
+            address: "",
+            zip: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.useremail) {
+              errors.useremail = true;
             }
-          ).then((res) => {
-            if (res.ok) {
-              alert("Your order was sent!");
-              setSubmitting(false);
-              resetForm();
-              cleanClothes();
-            } else {
-              alert("Something went wrong!");
-              setSubmitting(false);
-              resetForm();
-              cleanClothes();
-            }
-          });
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isValid,
-          dirty,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <div className="input-section">
-              <label htmlFor="username">Full name</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Enter your full name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.username}
-                className={errors.username && touched.username ? "error" : ""}
-              />
-            </div>
-            <div className="input-section">
-              <label htmlFor="useremail">E-mail</label>
-              <input
-                type="email"
-                name="useremail"
-                id="useremail"
-                placeholder="Enter email here"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.useremail}
-                className={errors.useremail && touched.useremail ? "error" : ""}
-              />
-            </div>
-            <div className="input-section">
-              <label htmlFor="address">Address</label>
-              <input
-                type="text"
-                name="address"
-                id="address"
-                placeholder="Enter your address"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.address}
-                className={errors.address && touched.address ? "error" : ""}
-              />
-            </div>
-            <div className="input-section">
-              <label htmlFor="zip">ZIP-code</label>
-              <input
-                type="number"
-                name="zip"
-                id="zip"
-                placeholder="Your location zip code"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.zip}
-                className={errors.zip && touched.zip ? "error" : ""}
-              />
-            </div>
-            <p className="total"> Total Sum: ${total}</p>
-            <button type="submit" disabled={!isValid || !dirty}>
-              Buy
-            </button>
 
-            <Persist name="signup-form" />
-          </form>
-        )}
-      </Formik>
-    </div>
+            if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.useremail)
+            ) {
+              errors.useremail = true;
+            }
+
+            if (!values.username) {
+              errors.username = true;
+            }
+
+            if (!values.address) {
+              errors.address = true;
+            }
+
+            if (!values.zip) {
+              errors.zip = true;
+            } else if (/\D/.test(values.zip)) {
+              errors.zip = true;
+            }
+
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            const data = new FormData();
+            data.append("username", values.username);
+            data.append("useremail", values.useremail);
+            data.append("address", values.address);
+            data.append("zip", values.zip);
+            data.append("total", total.toFixed(2));
+            data.append("clothes", composeString(clothes));
+            fetch(
+              "https://dsa1mc.wp4.host/wp-json/contact-form-7/v1/contact-forms/91/feedback",
+              {
+                method: "POST",
+                body: data,
+              }
+            ).then((res) => {
+              if (res.ok) {
+                alert("Your order was sent!");
+                setSubmitting(false);
+                resetForm();
+                cleanClothes();
+              } else {
+                alert("Something went wrong!");
+                setSubmitting(false);
+                resetForm();
+                cleanClothes();
+              }
+            });
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isValid,
+            dirty,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="input-section">
+                <label htmlFor="username">Full name</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Enter your full name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.username}
+                  className={errors.username && touched.username ? "error" : ""}
+                />
+              </div>
+              <div className="input-section">
+                <label htmlFor="useremail">E-mail</label>
+                <input
+                  type="email"
+                  name="useremail"
+                  id="useremail"
+                  placeholder="Enter email here"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.useremail}
+                  className={
+                    errors.useremail && touched.useremail ? "error" : ""
+                  }
+                />
+              </div>
+              <div className="input-section">
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  id="address"
+                  placeholder="Enter your address"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.address}
+                  className={errors.address && touched.address ? "error" : ""}
+                />
+              </div>
+              <div className="input-section">
+                <label htmlFor="zip">ZIP-code</label>
+                <input
+                  type="number"
+                  name="zip"
+                  id="zip"
+                  placeholder="Your location zip code"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.zip}
+                  className={errors.zip && touched.zip ? "error" : ""}
+                />
+              </div>
+              <p className="total"> Total Sum: ${total}</p>
+              <button type="submit" disabled={!isValid || !dirty}>
+                Buy
+              </button>
+
+              <Persist name="signup-form" />
+            </form>
+          )}
+        </Formik>
+      </div>
+      <Coupon setCouponDiscount={setCouponDiscount} />
+    </>
   );
 }
